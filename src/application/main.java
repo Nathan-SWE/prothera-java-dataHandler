b96bc3fb5735ba5346a91f61865c40fa61921ed8
Nathan Lima <nathanls.swe@gmail.com>
2025-04-02 20:47:22 -0300

    feat: acessa e imprime o conteudo do arquivo JSON

--- src/application/main.java
+++ src/application/main.java
@@ -1,5 +1,34 @@
 package application;
 
-public class main {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class main {
+  public static void main(String[] args) {
+    String jsonFilePath = "src/data/dados.json";
+    String jsonContent = lerArquivoJson(jsonFilePath);
+
+    if (jsonContent != null) {
+      System.out.println("JSON carregado com sucesso!");
+      System.out.println(jsonContent);
+    } else {
+      System.out.println("Erro ao carregar o JSON");
+    }
+  }
+
+  public static String lerArquivoJson(String caminho) {
+    StringBuilder conteudo = new StringBuilder();
+    try (BufferedReader br = new BufferedReader(new FileReader(caminho))){
+      String linha;
+
+      while ((linha = br.readLine()) != null) {
+        conteudo.append(linha);        
+      }
+      return conteudo.toString();
+    } catch (IOException error) {
+      error.printStackTrace();
+      return null;
+    }
+  }
 }
