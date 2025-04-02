92507afcf8bc34d7cf6ba1b032b6fbe36aaf904b
Nathan Lima <nathanls.swe@gmail.com>
2025-04-02 20:33:53 -0300

    feat: implementa classe Documento

--- src/entities/Documento.java
+++ src/entities/Documento.java
@@ -1,5 +1,29 @@
 package entities;
 
 public class Documento {
+  private String tipo;
+  private String numero;
+  private String descricao;
 
+  public Documento(String tipo, String numero, String descricao) {
+    this.tipo = tipo;
+    this.numero = numero;
+    this.descricao = descricao;
+  }
+
+  public String getTipo() {
+    return tipo;
+  }
+
+  public String getNumero() {
+    return numero;
+  }
+
+  public String getDescricao() {
+    return descricao;
+  }
+
+  public String toString() {
+    return String.format("Documento{tipo: '%s', numero: '%s', descricao: '%s'}", tipo, numero, descricao);
+  }
 }
