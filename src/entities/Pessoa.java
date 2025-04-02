12975f0c06420b654821e50813598bdd64c7ce0d
Nathan Lima <nathanls.swe@gmail.com>
2025-04-02 20:27:38 -0300

    feat: implementa classe Pessoa

--- src/entities/Pessoa.java
+++ src/entities/Pessoa.java
@@ -1,5 +1,37 @@
 package entities;
 
-public class Pessoa {
+import java.util.List;
 
+public class Pessoa {
+  private int id;
+  private String nome;
+  private int idade;
+  private List<Documento> documentos;
+
+  public Pessoa(int id, String nome, int idade, List<Documento> documentos){
+    this.id = id;
+    this.nome = nome;
+    this.idade = idade;
+    this.documentos = documentos;
+  }
+
+  public int getId() {
+    return id;  
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public int getIdade() {
+    return idade;
+  }
+
+  public List<Documento> getDocumentos() {
+    return documentos;
+  }
+
+  public String toString() {
+    return String.format("Pessoa{id: %d, nome: '%s', idade: %d, documentos: %s}", id, nome, idade, documentos);
+  }
 }
